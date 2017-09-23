d4cdaa6c2b47888b8dcc15c7656ff7e647c39775
mungaiandela <martin.mungai@andela.com>
2017-09-23 18:10:45 +0300

    [Feature] Create models file

--- app/models.py
+++ app/models.py
@@ -1,63 +1,30 @@
+from datetime import datetime
+
 from flask_sqlalchemy import SQLAlchemy
-from app.views import app
-from werkzeug.security import generate_password_hash, check_password_hash
 
-
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
 class User(db.Model):
-    # Table which stores all the user information during registration
-    # Instantiate
-    def __init__(self, username, firstname, lastname, email, password):
-        self.username = username
-        self.firstname = firstname
-        self.lastname = lastname
-        self.email = email
-        self.password = password
 
-    __tablename__ = 'table_users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(20), unique=True)
-    firstname = db.Column(db.String(20))
-    lastname = db.Column(db.String(20))
-    email = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(20))
+    #Model to store user emails
+    __tablename__ = "User"
 
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(25))
+    email = db.Column(db.String(40), nullable=False)
 
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
 
-    def is_authenticated(self):
-        return True
+class Todo(db.Model):
 
-    def is_active(self):
-        return True
+    #Model to store tasks
+    __tablename__ = "Todo"
 
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        return unicode(self.email)
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(25), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
 
     def __repr__(self):
-        return "<User(firstname='%s', lastname = '%s', email = '%s')>"(self.firstname, self.lastname, self.email)
+        return '<Task %r>' % self.task_name
 
-
-class User_data(db.Model):
-    # Table which stores users to-do information
-
-    # Instantiate
-    def __init__(self, title, desc):
-        self.title = title
-        self.desc = desc
-
-    __tablename__ = 'table_users_data'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(20))
-    desc = db.Column(db.String(20))
-
-    def __repr__(self):
-        return "<User_data(title = '%s', desc = '%s')>"(self.title, self.desc)
